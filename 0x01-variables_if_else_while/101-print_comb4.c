4808c4e1d697045691f1a02696e44f18d2894845
ahmed taha <ahmedtaha010238@yahoo.com>
2023-03-17 18:05:44 -0700

    f

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+
+	for (; i <= 57; i++)
+	{
+		int j = i + 1;
+
+		for (; j <= 57; j++)
+		{
+			int k = j + 1;
+
+			for (; k <= 57; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != 55 || j != 56 || k != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
