62e6f3389514ed0f90ecbed73788e46862547ec7
ahmed taha <ahmedtaha010238@yahoo.com>
2023-03-17 17:28:11 -0700

    f

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+
+	for (; i <= 57; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
